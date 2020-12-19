ec21c66a30a8a5f048f083a4cda7204c3aee6042
karpust <956277@mail.ru>
2020-12-19 22:23:14 +0300

    practical tasks from lesson 4

--- task_6.py
+++ task_6.py
@@ -1,28 +1,27 @@
-"""6. Реализовать функцию int_func(), принимающую слово из маленьких
-латинских букв и возвращающую его же, но с прописной первой буквой.
-Например, print(int_func(‘text’)) -> Text.
-Продолжить работу над заданием. В программу должна попадать строка
-из слов, разделенных пробелом. Каждое слово состоит из латинских
-букв в нижнем регистре. Сделать вывод исходной строки, но каждое
-слово должно начинаться с заглавной буквы. Необходимо использовать
-написанную ранее функцию int_func()."""
+"""6. Реализовать два небольших скрипта:
+а) итератор, генерирующий целые числа, начиная с указанного,
+б) итератор, повторяющий элементы некоторого списка, определенного заранее.
+Подсказка: использовать функцию count() и cycle() модуля itertools. Обратите внимание,
+что создаваемый цикл не должен быть бесконечным. Необходимо предусмотреть условие его завершения.
+Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10
+завершаем цикл. Во втором также необходимо предусмотреть условие, при котором повторение
+элементов списка будет прекращено."""
 
-# input_string1 = input("Enter word by latin lowercase letters: ")
-input_string2 = input("Enter space separated words by latin lowercase letters: ")
-
-
-def cap_word(b):
-    return b.capitalize()
+from itertools import count, cycle
 
 
-def cap_string(s):
-    list_of_str = s.split(" ")
-    for i in range(len(list_of_str)):
-        list_of_str[i] = cap_word(list_of_str[i])
-    return " ".join(list_of_str)
+iter_coun = count(3)
+iter_cyc = cycle(["1", "2", "3"])
 
+for i in iter_coun:
+    print(i)
+    if i == 10:
+        break
 
-# print(cap_word(input_string1))
-print(cap_string(input_string2))
-
+lst = []
+for i in iter_cyc:
+    lst.append(i)
+    if len(lst) == 21:
+        break
+print(lst)
 
